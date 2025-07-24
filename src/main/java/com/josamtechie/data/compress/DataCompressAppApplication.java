88e90a7c03a73798e03cc8a8d50385d68c597ff4
JOMON SAMUEL THOMAS <josamvkd@gmail.com>
2025-07-24 13:33:42 +0400

    first commit

---
+++ src/main/java/com/josamtechie/data/compress/DataCompressAppApplication.java
@@ -0,0 +1,13 @@
+package com.josamtechie.data.compress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCompressAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCompressAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josamtechie/data/compress/DataCompressAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josamtechie.data.compress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCompressAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
